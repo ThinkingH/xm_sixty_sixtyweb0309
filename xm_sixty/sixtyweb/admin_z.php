575c31682582a918d180ac3ecfdde400780ff4d4
lirongqi <lirongqi@03d0cfb7-df8a-42be-a7f1-cf5d3854756b>
2018-03-09 08:50:52 +0000

    更新admin_z.php

--- xm_sixty/sixtyweb/admin_z.php
+++ xm_sixty/sixtyweb/admin_z.php
@@ -18,15 +18,6 @@
 	define('HY_NEI_URLIP','http://127.0.0.1:8009/');
 	
 	//七牛bucket定义存储正式
-//	$bucketarr = array(
-//		'sixty-basic'      => 'http://oys7hzyf8.bkt.clouddn.com/',  //基础公共图片存放，循环展示图片，默认图片等公共静态资源图片
-//		'sixty-user'       => 'http://oys7i4dcy.bkt.clouddn.com/',  //用户图片存放，头像，用户其他数据
-//		'sixty-video'      => 'http://oys78eqga.bkt.clouddn.com/',  //视频存放
-//		'sixty-videoimage' => 'http://oys7tcwkg.bkt.clouddn.com/',  //视频封面图片存放
-//		'sixty-imgpinglun' => 'http://oys72yckt.bkt.clouddn.com/',  //带图片投稿评论存放
-//		'sixty-jihemsg'    => 'http://oys7xme11.bkt.clouddn.com/',  //集合封面图片存放
-//	);
-//七牛bucket定义存储 测试用
 $bucketarr = array(
     'sixty-basic'      => 'http://p05s45h9l.bkt.clouddn.com/',  //基础公共图片存放，循环展示图片，默认图片等公共静态资源图片
     'sixty-user'       => 'http://p05srrm5u.bkt.clouddn.com/',  //用户图片存放，头像，用户其他数据
@@ -36,6 +27,16 @@ $bucketarr = array(
     'sixty-jihemsg'    => 'http://p05svs60z.bkt.clouddn.com/',  //集合封面图片存放
 
 );
+//七牛bucket定义存储 本地测试用
+// $bucketarr = array(
+//     'sixty-basic'      => 'http://ox1bxmb6a.bkt.clouddn.com/',
+//     'sixty-user'       => 'http://ox1br6s4h.bkt.clouddn.com/',
+//     'sixty-video'      => 'http://ox1b34md9.bkt.clouddn.com/',
+//     'sixty-videoimage' => 'http://ox1cq2koa.bkt.clouddn.com/',
+//     'sixty-imgpinglun' => 'http://oy1shb1ug.bkt.clouddn.com/',
+//     'sixty-jihemsg'    => 'http://oy24w69i6.bkt.clouddn.com/',
+
+// );
 	$bucketstr = json_encode($bucketarr);
 	define('QINIUBUCKETSTR',$bucketstr);
 	
